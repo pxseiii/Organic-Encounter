1505864ade54e3f60072d9cf4adad8caefaa591d
kimee-ko <somethinginearth@gmail.com>
2025-12-19 22:18:34 +0800

    Add files via upload

--- OrganicEncounter/view/TitlePanel.java
+++ OrganicEncounter/view/TitlePanel.java
@@ -10,7 +10,7 @@ public class TitlePanel{
 
     public TitlePanel(){
         mainPanel = new JPanel(null){
-            private Image background = new ImageIcon("OrganicEncounter/images/bg_flat_v1_colored.png").getImage();
+            private Image background = new ImageIcon("images/bg_flat_v1_colored.png").getImage();
         
             @Override
             protected void paintComponent(Graphics g){
@@ -19,20 +19,39 @@ public class TitlePanel{
             }
         };
 
-        ImageIcon buttonDefault = new ImageIcon("OrganicEncounter/images/1.png");
-        ImageIcon buttonPressed = new ImageIcon("OrganicEncounter/images/2.png");
-        // ImageIcon buttonHover = new ImageIcon("");
+        int width=220;
+        int height = 56;
 
-        startButton = new JButton(buttonDefault);
+        ImageIcon buttonDefault = new ImageIcon("images/normal.png");
+        Image scaledButt = buttonDefault.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        ImageIcon buttonPressed = new ImageIcon("images/pressed.png");
+        Image scaledButtP = buttonPressed.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        ImageIcon buttonHover = new ImageIcon("images/hovered.png");
+        Image scaledButtH = buttonHover.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+
+        startButton = new JButton(new ImageIcon(scaledButt));
         startButton.setBorderPainted(false);
         startButton.setContentAreaFilled(false);
         startButton.setFocusPainted(false);
-        // startButton.setRolloverIcon(buttonHover);
-        startButton.setPressedIcon(buttonPressed);
 
-        startButton.setBounds(150, 500, 200, 75);
+        startButton.setRolloverIcon(new ImageIcon(scaledButtP));
+        startButton.setPressedIcon(new ImageIcon(scaledButtH));
+
+        int iconSize = 295;
+
+        ImageIcon icon = new ImageIcon("images/logo.png");
+        Image scaledIcon = icon.getImage().getScaledInstance(iconSize,iconSize+15, Image.SCALE_SMOOTH);
+        
+        JLabel iconLabel = new JLabel(new ImageIcon(scaledIcon));
+        iconLabel.setBounds(95, 50,iconSize ,iconSize);
+
+
+        startButton.setBounds(135, 360, width, height);
+        //icon.setBounds(125, 0, 250,250);
 
         mainPanel.add(startButton);
+        mainPanel.add(iconLabel);
+        mainPanel.setBackground(new Color(0xe3e9d8));
     }
 
     public JPanel getPanel(){
