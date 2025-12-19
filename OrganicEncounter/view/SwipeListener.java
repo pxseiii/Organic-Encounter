74dbcd4117ab7c08086e548679edbff305c5ec95
kimee-ko <somethinginearth@gmail.com>
2025-12-20 02:50:25 +0800

    added annotations

--- OrganicEncounter/view/SwipeListener.java
+++ OrganicEncounter/view/SwipeListener.java
@@ -1,4 +1,12 @@
 package view;
+import annotation.ClassInfo;
+
+@ClassInfo(
+    mainAuthor = "",
+    className = "SwipeListener",
+    pillarsUsed = {},
+    solidUsed = {}
+)
 
 // for user-input only, Presenter implements the interface
 public interface SwipeListener {
