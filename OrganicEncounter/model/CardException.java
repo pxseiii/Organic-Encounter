5f3ed1a72f83bb3965da8bedfb25f22d634a814d
kimee-ko <somethinginearth@gmail.com>
2025-12-20 02:39:20 +0800

    added annotation

--- OrganicEncounter/model/CardException.java
+++ OrganicEncounter/model/CardException.java
@@ -1,5 +1,14 @@
 package model;
 
+import annotation.ClassInfo;
+
+@ClassInfo(
+    mainAuthor = "Mika",
+    className = "CardException",
+    pillarsUsed = {},
+    solidUsed = {"SRP"}
+)
+
 class CardException extends RuntimeException{
     public CardException() {}
 
